a1874f7dada9df67450026c77f8f1281133fc538
Navor Nunez <navor.nunez@solardesigntool.com>
2021-04-14 23:57:52 -0400

    Add navegation bar and proejct board templates.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+import ProjectBoard from './components/ProjectBoard';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the project board.</h1>
+      <NavBar />
+      <ProjectBoard />
     </div>
   );
 }
