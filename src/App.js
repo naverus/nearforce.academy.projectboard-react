67e8c40c75bff5a987eb3b7f2619ee0474582d52
Navor Nunez <navor.nunez@solardesigntool.com>
2021-04-15 23:06:47 -0400

    Add initial routing logic.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
-import ProjectBoard from './components/ProjectBoard';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import ProjectBoard from "./components/ProjectBoard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProjectTask from "./components/ProjectTask/AddProjectTask";
+import UpdateProjectTask from "./components/ProjectTask/UpdateProjectTask";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <ProjectBoard />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Route exact path="/" component={ProjectBoard} />
+        <Route exact path="/addProjectTask" component={AddProjectTask} />
+        <Route exact path="/updateProjectTask" component={UpdateProjectTask} />
+      </div>
+    </Router>
   );
 }
 
